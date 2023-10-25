09049a92da3fc496fbd8edec598616ae08b55451
Gilles Bruno <gilles_bruno@gmx.fr>
2023-10-25 15:35:10 +0200

    Add bueatiful buttons on home page + others

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,22 +1,47 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
   return (
     <section>
-        <div className='grid grid-cols-1 lg:grid-cols-12'>
-            <div className='col-span-7 place-self-center'>
-                <h1 className='mb-4 text-4xl font-extrabold text-white'>Hello, I'm Gilles</h1>
-                <p className='text-[#ADB7BE] text-lg lg:text-xl mb-6'>
-                    paragraphe wdfhxfhxfdxdf  
-                    wdrgxdfhxfghfghfghxfghcyhcghjvhjhk
+        <div className='grid grid-cols-1 sm:grid-cols-12'>
+            <div className='col-span-7 text-center place-self-center sm:text-left'>
+                <h1 className='mb-4 text-4xl font-extrabold text-white sm:text-5xl lg:text-6xl'> 
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-700 via-orange-700 to-yellow-200'>
+                        Hello, I'm {" "}
+                    </span>
+                    <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'Gilles',
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        'Full stack dev',
+                        1000,
+                        'Web3 dev',
+                        1000,
+                        'trainer in engineers schools',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style={{ fontSize: '1em', display: 'inline-block' }}
+                    repeat={Infinity}
+                    />
+                </h1>
+                <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6'>
+                    Full stack dev, trainer in many engineers schools
                 </p>    
                 <div>
-                    <button className='px-6 py-3 mr-4 text-black bg-white rounded-full hover:bg-slate-200'>
+                    <button className='w-full px-6 py-3 mb-3 mr-4 text-white bg-white rounded-full sm:w-fit hover:bg-slate-200 bg-gradient-to-br from-red-700 via-orange-700 to-yellow-200'>
                         Hire Me
                     </button>
-                    <button className='px-6 py-3 mt-3 text-white bg-transparent border border-white rounded-full hover:bg-slate-800'>
+                    <button className='w-full px-1 py-1 text-white bg-transparent rounded-full sm:w-fit py-w-full hover:bg-slate-800 bg-gradient-to-br from-red-700 via-orange-700 to-yellow-200'>
+                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                         Download CV
+                        </span>
+                        
                     </button>
                 </div>
             </div>
--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen bg-[#121212] container mx-auto px-12 py-4">
-      <HeroSection>
-
-      </HeroSection>
+    <main className="flex flex-col min-h-screen bg-[#121212]">
+      <div className='container px-12 py-4 mx-auto'>
+        <HeroSection/>
+      </div>
     </main>
   )
 }
